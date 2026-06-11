398a6129610387a3a90cbf6662e940a4c6091daf
Karotte128 <karotte128@outlook.de>
2026-06-11 20:46:55 +0200

    split up ssh server logic + simple handler

--- main.go
+++ main.go
@@ -1,119 +1,53 @@
 package main
 
 import (
-	"errors"
+	"bufio"
 	"log"
-	"net"
-	"os"
-	"path/filepath"
-	"strconv"
+	"ssh-go/sshsrv"
 
 	"golang.org/x/crypto/ssh"
 )
 
-func handleServerConn(channels <-chan ssh.NewChannel) {
-	for newChannel := range channels {
-		if newChannel.ChannelType() != "session" {
-			_ = newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
-			continue
-		}
+func runShell(ch ssh.Channel) {
+	defer ch.Close()
 
-		ch, reqs, err := newChannel.Accept()
-		if err != nil {
-			log.Printf("Error accepting channel: %v", err)
-			continue
-		}
-
-		go func(in <-chan *ssh.Request) {
-			defer func() {
-				_ = ch.Close()
-			}()
-
-			for req := range in {
-				log.Printf("request: %s wantReply=%v payload=%x", req.Type, req.WantReply, req.Payload)
-				req.Reply(true, nil)
-				// do something?
-			}
-		}(reqs)
-	}
-}
-
-func listen(config *ssh.ServerConfig, port int) {
-	listener, err := net.Listen("tcp", "0.0.0.0:"+strconv.Itoa(port))
-	if err != nil {
-		panic(err)
-	}
+	scanner := bufio.NewScanner(ch)
 
 	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("Connection error: %v", err)
-			continue
+		ch.Write([]byte("> "))
+
+		if !scanner.Scan() {
+			log.Println("scanner ended")
+			return
 		}
 
-		_, chans, reqs, err := ssh.NewServerConn(conn, config)
-		if err != nil {
-			log.Printf("Handshake error: %v", err)
-			continue
+		if err := scanner.Err(); err != nil {
+			log.Println(err)
 		}
 
-		go ssh.DiscardRequests(reqs)
-		go handleServerConn(chans)
+		log.Printf("received: %q", scanner.Text())
 	}
 }
 
-func authorizePublicKey(key ssh.PublicKey) (bool, error) {
-	fp := ssh.FingerprintSHA256(key)
-	log.Println(fp)
-	return true, nil // TODO: check if public key is authorized
-}
+func handleSession(ch ssh.Channel, reqs <-chan *ssh.Request) {
+	for req := range reqs {
+		switch req.Type {
 
-func getHostKeys() ([]ssh.Signer, error) {
-	var hostKeys []ssh.Signer
+		case "pty-req":
+			req.Reply(true, nil)
 
-	keyPath := filepath.Join(".", "ssh", "key")
+		case "shell":
+			req.Reply(true, nil)
 
-	keyData, err := os.ReadFile(keyPath)
-	if err != nil {
-		return nil, err
+			go runShell(ch)
+
+		default:
+			req.Reply(false, nil)
+			log.Printf("Request type %v not implemented!", req.Type)
+		}
 	}
-	signer, err := ssh.ParsePrivateKey(keyData)
-	if err != nil {
-		return nil, err
-	}
-
-	hostKeys = append(hostKeys, signer)
-	return hostKeys, nil
-}
-
-func RunServer(port int) {
-	config := &ssh.ServerConfig{
-		PublicKeyCallback: func(conn ssh.ConnMetadata, key ssh.PublicKey) (*ssh.Permissions, error) {
-			authorized, err := authorizePublicKey(key)
-			if err != nil {
-				return nil, err
-			}
-
-			if authorized {
-				return &ssh.Permissions{}, nil
-			} else {
-				return nil, errors.New("unauthorized")
-			}
-
-		},
-	}
-
-	keys, err := getHostKeys()
-	if err != nil {
-		log.Fatalf("Error loading host keys: %v", err)
-	}
-	for _, key := range keys {
-		config.AddHostKey(key)
-	}
-
-	listen(config, port)
 }
 
 func main() {
-	RunServer(2222)
+	sshsrv.RunServer(2222, handleSession)
 }
---
+++ sshsrv/server.go
@@ -0,0 +1,111 @@
+package sshsrv
+
+import (
+	"errors"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"golang.org/x/crypto/ssh"
+)
+
+type sessionHandler func(ch ssh.Channel, reqs <-chan *ssh.Request)
+
+var sessionHandlerFunc sessionHandler
+
+func handleServerConn(channels <-chan ssh.NewChannel) {
+	for newChannel := range channels {
+		if newChannel.ChannelType() != "session" {
+			_ = newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
+			continue
+		}
+
+		ch, reqs, err := newChannel.Accept()
+		if err != nil {
+			log.Printf("Error accepting channel: %v", err)
+			continue
+		}
+
+		go sessionHandlerFunc(ch, reqs)
+	}
+}
+
+func listen(config *ssh.ServerConfig, port int) {
+	listener, err := net.Listen("tcp", "0.0.0.0:"+strconv.Itoa(port))
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Connection error: %v", err)
+			continue
+		}
+
+		_, chans, reqs, err := ssh.NewServerConn(conn, config)
+		if err != nil {
+			log.Printf("Handshake error: %v", err)
+			continue
+		}
+
+		go ssh.DiscardRequests(reqs)
+		go handleServerConn(chans)
+	}
+}
+
+func authorizePublicKey(key ssh.PublicKey) (bool, error) {
+	fp := ssh.FingerprintSHA256(key)
+	log.Println(fp)
+	return true, nil // TODO: check if public key is authorized
+}
+
+func getHostKeys() ([]ssh.Signer, error) {
+	var hostKeys []ssh.Signer
+
+	keyPath := filepath.Join(".ssh", "key")
+
+	keyData, err := os.ReadFile(keyPath)
+	if err != nil {
+		return nil, err
+	}
+	signer, err := ssh.ParsePrivateKey(keyData)
+	if err != nil {
+		return nil, err
+	}
+
+	hostKeys = append(hostKeys, signer)
+	return hostKeys, nil
+}
+
+func RunServer(port int, sh sessionHandler) {
+	config := &ssh.ServerConfig{
+		PublicKeyCallback: func(conn ssh.ConnMetadata, key ssh.PublicKey) (*ssh.Permissions, error) {
+			authorized, err := authorizePublicKey(key)
+			if err != nil {
+				return nil, err
+			}
+
+			if authorized {
+				return &ssh.Permissions{}, nil
+			} else {
+				return nil, errors.New("unauthorized")
+			}
+
+		},
+	}
+
+	keys, err := getHostKeys()
+	if err != nil {
+		log.Fatalf("Error loading host keys: %v", err)
+	}
+	for _, key := range keys {
+		config.AddHostKey(key)
+	}
+
+	sessionHandlerFunc = sh
+
+	listen(config, port)
+}
